3b7da1d6b6ca2d8b7ea90131c8722ceef4455913
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2024-06-24 21:46:20 +0530

    Merge Sorted Array

---
+++ Merge Sorted Array.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        
+        result = nums1[:m] + nums2[:n]
+        # [result.append(nums1[i]) for i in range(m)]
+        # [result.append(nums2[i]) for i in range(n)]
+        
+        result.sort()
+        
+        for i in range(m+n):
+            nums1[i] = result[i]
+
+'''
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+Explanation: The arrays we are merging are [1] and [].
+The result of the merge is [1].
+'''
