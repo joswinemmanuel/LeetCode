936b08f27e943c93142cc7803ca0ddf52bf49321
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2024-06-24 20:25:38 +0530

    Ransom Note

---
+++ 383. Ransom Note.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        st1, st2 = Counter(ransomNote), Counter(magazine)
+        if st1 & st2 == st1:
+            return True
+        return False
+        
+'''
+Input: ransomNote = "a", magazine = "b"
+Output: false
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+'''
