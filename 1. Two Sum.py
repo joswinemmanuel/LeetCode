c0762903ab3fc99be392f2e60962135c35f1120c
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2024-06-25 09:43:36 +0530

    Two Sum

---
+++ 1. Two Sum.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1, n):
+                if nums[i]+nums[j] == target:
+                    return [i, j]
+
+'''
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+'''
