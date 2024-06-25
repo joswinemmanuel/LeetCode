26cb9b8a51edfcea3dc8da5f198c863257a9260c
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2024-06-25 09:52:06 +0530

    Remove Element

---
+++ 27. Remove Element.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        while val in nums:
+            nums.remove(val)
+        
+        return len(nums)
+
+
+'''
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+Input: nums = [0,1,2,2,3,0,4,2], val = 2
+Output: 5, nums = [0,1,4,0,3,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+Note that the five elements can be returned in any order.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+'''
