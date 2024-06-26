9247475d8845deb2fb65d0a1ef5f47c7c2e35c09
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2024-06-26 12:15:51 +0530

    7. Reverse Integer

---
+++ 7. Reverse Integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long r=0;
+        while(x) {
+            r=r*10+x%10;
+            x=x/10;
+        }
+        if(r>INT_MAX || r<INT_MIN) return 0;
+        return int(r);
+    }
+};
+
+/*
+Input: x = 123
+Output: 321
+
+Input: x = -123
+Output: -321
+
+Input: x = 120
+Output: 21
+*/
