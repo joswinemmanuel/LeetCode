277269a4e52fc9d4dbf88503eb28f7de92a2e571
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2024-06-26 12:34:49 +0530

    Combine Two Tables

---
+++ 175. Combine Two Tables.sql
@@ -0,0 +1,31 @@
+select firstName, lastName, city, state
+from Person Left join Address
+on Person.personId = Address.personId;
+
+/*
+Input: 
+Person table:
++----------+----------+-----------+
+| personId | lastName | firstName |
++----------+----------+-----------+
+| 1        | Wang     | Allen     |
+| 2        | Alice    | Bob       |
++----------+----------+-----------+
+Address table:
++-----------+----------+---------------+------------+
+| addressId | personId | city          | state      |
++-----------+----------+---------------+------------+
+| 1         | 2        | New York City | New York   |
+| 2         | 3        | Leetcode      | California |
++-----------+----------+---------------+------------+
+Output: 
++-----------+----------+---------------+----------+
+| firstName | lastName | city          | state    |
++-----------+----------+---------------+----------+
+| Allen     | Wang     | Null          | Null     |
+| Bob       | Alice    | New York City | New York |
++-----------+----------+---------------+----------+
+Explanation: 
+There is no address in the address table for the personId = 1 so we return null in their city and state.
+addressId = 1 contains information about the address of personId = 2.
+*/
