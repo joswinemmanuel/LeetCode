b6d0c0f669b6d08e476eabd5cce7083dfed1fb62
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2024-06-26 12:07:23 +0530

    Palindrome Number

---
+++ 9. Palindrome Number.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        return str(x)[::-1] == str(x)
+
+
+'''
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+'''
