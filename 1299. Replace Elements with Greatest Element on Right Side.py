991eb9ab0f885041bcb08bfc6612314c3ea61a90
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2024-06-27 08:38:35 +0530

    Replace Elements with Greatest Element on Right Side

---
+++ 1299. Replace Elements with Greatest Element on Right Side.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def replaceElements(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[int]
+        """
+        n = len(arr)
+        gtr = -1
+        temp = arr[n-1]
+        for i in range(n-1, -1, -1):
+            temp = arr[i]
+            arr[i] = gtr
+            gtr = max(gtr, temp)
+        return arr
+            
+
+'''
+Input: arr = [17,18,5,4,6,1]
+Output: [18,6,6,6,1,-1]
+Explanation: 
+- index 0 --> the greatest element to the right of index 0 is index 1 (18).
+- index 1 --> the greatest element to the right of index 1 is index 4 (6).
+- index 2 --> the greatest element to the right of index 2 is index 4 (6).
+- index 3 --> the greatest element to the right of index 3 is index 4 (6).
+- index 4 --> the greatest element to the right of index 4 is index 5 (1).
+- index 5 --> there are no elements to the right of index 5, so we put -1.
+
+Input: arr = [400]
+Output: [-1]
+Explanation: There are no elements to the right of index 0.
+'''
