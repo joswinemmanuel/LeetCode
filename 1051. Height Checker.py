301e5c30ab037cf7655ae032a9657708aed15206
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2024-06-27 09:02:07 +0530

    1051. Height Checker

---
+++ 1051. Height Checker.py
@@ -0,0 +1,32 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        count = 0
+        l = sorted(heights)
+        for i in range(len(heights)):
+            if heights[i] != l[i]:
+                count += 1
+        return count
+
+
+'''
+Input: heights = [1,1,4,2,1,3]
+Output: 3
+Explanation: 
+heights:  [1,1,4,2,1,3]
+expected: [1,1,1,2,3,4]
+Indices 2, 4, and 5 do not match.
+
+Input: heights = [5,1,2,3,4]
+Output: 5
+Explanation:
+heights:  [5,1,2,3,4]
+expected: [1,2,3,4,5]
+All indices do not match.
+
+Input: heights = [1,2,3,4,5]
+Output: 0
+Explanation:
+heights:  [1,2,3,4,5]
+expected: [1,2,3,4,5]
+All indices match.
+'''
